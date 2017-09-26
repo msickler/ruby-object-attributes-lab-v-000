58d64ef9a34bb04ae45231511bcd7ad2fa807133
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-26 17:51:08 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 class Dog
-  def name(dog_name)
-    @name = dog_name
+  def name
+    @name
   end
 
   def name=(dog_name)
@@ -9,7 +9,7 @@ class Dog
 def breed=(breed_name)
   @breed = breed_name
 end
-def breed(breed_name)
+def breed
   @breed
 end
 end
